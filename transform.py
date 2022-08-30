b3933e8a3044affda42080ab3e596b206affc01d
AlbertoLandinWork <albertolandinwork@gmail.com>
2022-08-29 22:07:11 -0500

    Se terminó la implementación de los modelos, queda pendiente ajuste de hiperparametros

--- transform.py
+++ transform.py
@@ -4,9 +4,11 @@ import pandas as pd
 def transform(path: str)-> pd.DataFrame:
     data_frame = pd.read_csv(path)
 
-
+    # Elimino columnas sobrantes
     data_frame.drop(columns = ['unit', 'obs_status', 'decimal'], inplace = True)
+    # Renombro columnas utiles
     data_frame.rename(columns = {'countryiso3code':'country_code', 'value': 'population', 'date': 'year'}, inplace = True)
+    # Elimino valores nulos
     data_frame.dropna(subset = ['country_code', 'population'], inplace = True)
 
 
